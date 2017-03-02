69c0f403a9faaad4b80432e45e58048a9f166dff
Stephan Preßl <mail@stephanpressl.at>
2017-03-02 17:15:29 +0100

    set contao manager plugin files

---
+++ ContaoManager/Plugin.php
@@ -0,0 +1,37 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ *******************************************************************/
+
+namespace IIDO\BasicBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Stephan Preßl <development@prestep.at>
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('IIDO\BasicBundle\IIDOBasicBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+                ->setReplace(['iido_basic']),
+        ];
+    }
+}
