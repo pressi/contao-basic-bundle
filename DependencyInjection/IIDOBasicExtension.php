269d8afce69737a25d1b73462a48fd57243695ae
Stephan Preßl <mail@stephanpressl.at>
2017-03-02 17:05:45 +0100

    init bundle config files

---
+++ DependencyInjection/IIDOBasicExtension.php
@@ -0,0 +1,39 @@
+<?php
+/******************************************************************
+ *
+ * (c) 2016 Stephan Preßl <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ ******************************************************************/
+
+namespace IIDO\BasicBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Configures the Contao IIDO Basic Bundle.
+ *
+ * @author Stephan Preßl <https://github.com/pressi>
+ */
+class IIDOBasicExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('listeners.yml');
+    }
+}
---
+++ IIDOBasicBundle.php
@@ -0,0 +1,34 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ *******************************************************************/
+
+namespace IIDO\BasicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use IIDO\BasicBundle\DependencyInjection\IIDOBasicExtension;
+
+/**
+ * Configures the Contao IIDO Basic Bundle.
+ *
+ * @author Stephan Preßl <development@prestep.at>
+ */
+class IIDOBasicBundle extends Bundle
+{
+
+    /**
+     * Register extension
+     * @return \IIDO\BasicBundle\DependencyInjection\IIDOBasicExtension
+     */
+    public function getContainerExtension()
+    {
+        return new IIDOBasicExtension();
+    }
+}
