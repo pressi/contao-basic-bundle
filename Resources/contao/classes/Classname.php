7988ceb43951a1ca8e389233d714ffb7ee95c4a4
Stephan Preßl <mail@stephanpressl.at>
2017-03-02 17:01:20 +0100

    change composer.json and add default classes

---
+++ Resources/contao/classes/Classname.php
@@ -0,0 +1,18 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ *******************************************************************/
+
+namespace IIDO\BasicBundle;
+
+class Classname
+{
+
+}
