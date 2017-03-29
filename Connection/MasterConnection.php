56af0330c224384fd3342112962c8c4d23216ce4
Stephan Preßl <mail@stephanpressl.at>
2017-03-29 17:02:24 +0200

    add / edit connection
    edit MasterConnection
    edit master-connection.json
    add connection client

--- Connection/MasterConnection.php
+++ Connection/MasterConnection.php
@@ -7,23 +7,93 @@
  * www.iido.at <development@iido.at>
  *******************************************************************/
 
-namespace IIDO\BasciBundle\Connection;
+namespace IIDO\BasicBundle\Connection;
 
 use Contao\System;
+use IIDO\BasicBundle\Config\BundleConfig;
+
 
 class MasterConnection
 {
+    /**
+     *
+     * @var MasterConnection
+     */
+    private static $instance;
+    
+    
+    private static $password; // iido543master12
+
     static $configFile      = 'Resources/config/master-connection.json';
-    static $bundlePath      = 'vendor/2do/contaco-basic-bundle';
+    static $bundlePath;
+    
 
-
-    public static function getData()
+    private function __construct()
     {
-        $configData     = static::getConfigData();
-        $connectionUrl  = $configData->domain . $configData->connection->publicPath . $configData->connection->file;
+        static::$bundlePath = BundleConfig::getBundlePath();
+        
+        if( \Input::post("password") )
+        {
+            $this->setPassword( \Input::post("password") );
+        }
+
+        if( !strlen(static::$password) && !\Input::get("method") == "login" )
+        {
+            $this->validate( $this->getActionData("checkPassword", array(), true) );
+        }
+    }
+
+    
+    public static function getInstance()
+    {
+        if ( is_null( self::$instance ) )
+        {
+            self::$instance = new self();
+        }
+        
+        return self::$instance;
+    }
+    
+    
+    
+    public function setPassword( $pwd )
+    {
+        self::$password = md5( $pwd );
+    }
+
+
+
+    public function isPasswordValid()
+    {
+        $arrData = $this->getActionData("checkPassword", array(), true);
+
+        if( key_exists("ERROR", $arrData) )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    public function getData($returnAsArray = false)
+    {
+        $connectionUrl  = $this->getConnectionUrl();
+        $arrData        = file_get_contents( $connectionUrl );
+        $objData        = json_decode($arrData, $returnAsArray);
+
+        return $objData;
+    }
+
+
+
+    public function getActionData( $actionName, array $actionParams = array(), $returnAsArray = false)
+    {
+        $connectionUrl  = $this->getConnectionUrl() . '&act=' . $actionName . (count($actionParams)?'&':'') . implode('&', $actionParams);
         $arrData        = file_get_contents( $connectionUrl );
 
-        return $arrData;
+        return json_decode($arrData, $returnAsArray);
     }
 
 
@@ -32,5 +102,54 @@ class MasterConnection
     {
         $rootDir = dirname(System::getContainer()->getParameter('kernel.root_dir'));
         return json_decode(file_get_contents($rootDir . '/' . static::$bundlePath . '/' . static::$configFile));
+    }
+
+
+
+    public function redirectTo( $method = "", array $msg = array() )
+    {
+        if( count($msg) )
+        {
+            \Session::getInstance()->set("iidoMessage", $msg);
+        }
+
+        if( $method )
+        {
+            \Controller::redirect( \Controller::addToUrl("do=iidoConfigContao&amp;method=" . $method) );
+        }
+        else
+        {
+            \Controller::redirect( \Controller::addToUrl("do=iidoConfigContao") );
+        }
+    }
+
+
+    public static function redirect( $method = "", array $msg = array() )
+    {
+        self::redirectTo( $method, $msg );
+    }
+
+
+
+    protected function getConnectionUrl()
+    {
+        $configData = $this->getConfigData();
+        return $configData->domain . $configData->connection->publicPath . $configData->connection->file . '?pwd=' . static::$password;
+    }
+
+
+
+    protected function validate( array $arrData )
+    {
+        if( key_exists("ERROR", $arrData) )
+        {
+            if( $arrData['ERROR'] == 'No correct password.' )
+            {
+                if( !\Input::get("method") == "login" )
+                {
+                    \Controller::redirect( \Controller::addToUrl("do=iidoConfigContao&amp;method=login") );
+                }
+            }
+        }
     }
 }
---
+++ Resources/public/connection/client.php
@@ -0,0 +1,79 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *
+ *******************************************************************/
+
+use Contao\ManagerBundle\ContaoManager\Plugin as ManagerBundlePlugin;
+use Contao\ManagerBundle\HttpKernel\ContaoCache;
+use Contao\ManagerBundle\HttpKernel\ContaoKernel;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
+use \Contao\Config;
+
+
+/** @var Composer\Autoload\ClassLoader */
+$loader = require __DIR__.'/../../../../../autoload.php';
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+ManagerBundlePlugin::autoloadModules(__DIR__.'/../../../../../../system/modules');
+
+$kernel = new ContaoKernel('prod', false);
+$kernel->setRootDir(dirname(__DIR__).'/../../../../../../../app');
+
+// Enable the Symfony reverse proxy
+$kernel = new ContaoCache($kernel);
+Request::enableHttpMethodParameterOverride();
+
+// Handle the request
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+//$response->send();
+//$kernel->terminate($request, $response);
+
+class Connection {
+
+    var $password = 'iido345client21';
+
+    public function run()
+    {
+        // TODO: make a client class!!
+
+        if( \Input::get("pwd") == $this->password )
+        {
+            $packages = System::getContainer()->getParameter('kernel.packages');
+
+            $arrConfig = array
+            (
+                'config'    => array
+                (
+                    'title'     => Config::get("websiteTitle")
+                ),
+
+                'version'   => $packages['contao/core-bundle'],
+                'bundles'   => array_keys(\System::getContainer()->getParameter('kernel.bundles') ),
+                'packages'  => $packages
+            );
+
+
+            header('Content-Type: application/json');
+            echo json_encode($arrConfig);
+            exit;
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode(array('ERROR'=>'No correct Password'));
+        exit;
+    }
+
+}
+
+$connection = new Connection();
+$connection->run();
