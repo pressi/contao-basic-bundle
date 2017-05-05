97e32e784f5994f7822c4b5a5a9d4331a6386c45
Stephan Preßl <mail@stephanpressl.at>
2017-05-05 15:50:45 +0200

    meta wizard widget

--- Widget/MetaWizardWidget.php
+++ Widget/MetaWizardWidget.php
@@ -74,12 +74,44 @@ class MetaWizardWidget extends \MetaWizard
                     {
                         $strField = '<textarea name="' . $this->strId . '[' . $lang . '][' . $field . ']" id="ctrl_' . $field . '_' . $count . '" class="tl_textarea tl_text">' . \StringUtil::specialchars($meta[$field]) . '</textarea>';
                     }
+                    elseif( $attributes == "select" || preg_match('/^select_/', $attributes) )
+                    {
+                        $arrAttributes  = explode("_", $attributes);
+                        $arrOptions     = '';
+
+                        if( $arrAttributes[1] == "blank" )
+                        {
+                            $arrOptions = '<option value="">-</option>';
+                        }
+
+                        $arrOptions     .= $this->getSelectOptions($field, \StringUtil::specialchars($meta[$field]));
+
+                        $strField       = '<select name="' . $this->strId . '[' . $lang . '][' . $field . ']" id="ctrl_' . $field . '_' . $count . '" class="tl_select">' . $arrOptions . '</select>';
+                    }
                     else
                     {
-                        $strField = '<input type="text" name="' . $this->strId . '[' . $lang . '][' . $field . ']" id="ctrl_' . $field . '_' . $count . '" class="tl_text" value="' . \StringUtil::specialchars($meta[$field]) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+                        $arrAttributes  = explode("_", $attributes);
+
+                        if( $arrAttributes[1] == 4 )
+                        {
+                            $arrValue   = deserialize($meta[$field], TRUE);
+
+                            $fieldTop       = '<input type="text" name="' . $this->strId . '[' . $lang . '][' . $field . '][top]" id="ctrl_' . $field . '_' . $count . '_top" class="tl_text_trbl trbl_top" value="' . \StringUtil::specialchars($arrValue['top']) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+                            $fieldRight     = '<input type="text" name="' . $this->strId . '[' . $lang . '][' . $field . '][right]" id="ctrl_' . $field . '_' . $count . '_right" class="tl_text_trbl trbl_right" value="' . \StringUtil::specialchars($arrValue['right']) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+                            $fieldBottom    = '<input type="text" name="' . $this->strId . '[' . $lang . '][' . $field . '][bottom]" id="ctrl_' . $field . '_' . $count . '_bottom" class="tl_text_trbl trbl_bottom" value="' . \StringUtil::specialchars($arrValue['bottom']) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+                            $fieldLeft      = '<input type="text" name="' . $this->strId . '[' . $lang . '][' . $field . '][left]" id="ctrl_' . $field . '_' . $count . '_bottom" class="tl_text_trbl trbl_left" value="' . \StringUtil::specialchars($arrValue['left']) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+
+                            $fieldOptions   = '<select name="' . $this->strId . '[' . $lang . '][' . $field . '][unit]" class="tl_select_unit">' . $this->getSelectOptions('units', $arrValue['unit']) . '</select>';
+
+                            $strField       = $fieldTop . $fieldRight . $fieldBottom . $fieldLeft . $fieldOptions;
+                        }
+                        else
+                        {
+                            $strField   = '<input name="' . $this->strId . '[' . $lang . '][' . $field . ']" type="text" name="' . $this->strId . '[' . $lang . '][' . $field . ']" id="ctrl_' . $field . '_' . $count . '" class="tl_text" value="' . \StringUtil::specialchars($meta[$field]) . '"' . (!empty($attributes) ? ' ' . $attributes : '') . '>';
+                        }
                     }
 
-                    $return .= '<label for="ctrl_' . $field . '_' . $count . '">' . $GLOBALS['TL_LANG']['MSC']['aw_' . $field] . '</label> ' . $strField . '<br>';
+                    $return .= '<div class="meta-widget widget-' . $field . '"><label for="ctrl_' . $field . '_' . $count . '">' . $GLOBALS['TL_LANG']['MSC']['aw_' . $field] . '</label> ' . $strField . '</div>';
                 }
 
                 $return .= '
@@ -107,5 +139,65 @@ class MetaWizardWidget extends \MetaWizard
   </div>';
 
         return $return;
+    }
+
+
+
+    protected function getSelectOptions( $fieldName, $selectedField)
+    {
+        $strOptions = '';
+        $arrOptions = $GLOBALS['TL_LANG']['OPT'][ 'aw_' . $fieldName ];
+
+        if( preg_match('/position$/', $fieldName) )
+        {
+            $arrOptions = $GLOBALS['TL_LANG']['RSCE']['positions'];
+        }
+        elseif( $fieldName == "units" )
+        {
+            $arrOptions = $GLOBALS['TL_CSS_UNITS'];
+        }
+
+        foreach($arrOptions as $optionValue => $optionName)
+        {
+            $selected = '';
+            
+            if( $selectedField == $optionValue )
+            {
+                $selected = ' selected';
+            }
+            
+            $strOptions .= '<option value="' . $optionValue . '"' . $selected . '>' . $optionName . '</option>';
+        }
+
+        return $strOptions;
+    }
+
+
+
+
+    /**
+     * Trim the values and add new languages if necessary
+     *
+     * @param mixed $varInput
+     *
+     * @return mixed
+     */
+    public function validator($varInput)
+    {
+        foreach ($varInput as $k => $v)
+        {
+            if ($k != 'language')
+            {
+                foreach($v as $vk => $vv)
+                {
+                    if( is_array($vv) )
+                    {
+                        $varInput[ $k ][ $vk ] = serialize( $vv );
+                    }
+                }
+            }
+        }
+
+        return parent::validator( $varInput );
     }
 }
