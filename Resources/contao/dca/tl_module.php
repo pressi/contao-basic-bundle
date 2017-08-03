8cdd6ca9c9ea428e7531f76b1b20d5b4b2257fd5
Stephan Preßl <mail@stephanpressl.at>
2017-08-03 11:37:30 +0200

    change module dcc

--- Resources/contao/dca/tl_module.php
+++ Resources/contao/dca/tl_module.php
@@ -7,6 +7,18 @@
  * www.iido.at <development@iido.at>
  *******************************************************************/
 
+\Controller::loadDataContainer("tl_article");
+\Controller::loadLanguageFile("tl_article");
+
+
+
+/**
+ * Palettes
+ */
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['iido_inheritArticle'] = '{title_legend},name,headline,type;{config_legend},inheritColumn;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+
+
 
 /**
  * Fields
@@ -16,3 +28,5 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['master_ID'] = array
 (
     'sql'                       => "int(10) unsigned NOT NULL"
 );
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['inheritColumn'] = $GLOBALS['TL_DCA']['tl_article']['fields']['inColumn'];
