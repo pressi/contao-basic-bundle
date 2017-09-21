7d967e6aa4033f84295087fd65f5757cf1e9886d
Stephan Preßl <mail@stephanpressl.at>
2017-09-21 10:55:39 +0200

    table classes update

--- Table/AllTables.php
+++ Table/AllTables.php
@@ -1,80 +1,43 @@
 <?php
-/******************************************************************
+/*******************************************************************
  *
- * (c) 2016 Stephan Preßl <development@prestep.at>
+ * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
  * All rights reserved
  *
  * Modification, distribution or any other action on or with
  * this file is permitted unless explicitly granted by IIDO
  * www.iido.at <development@iido.at>
  *
- ******************************************************************/
+ *******************************************************************/
 
 namespace IIDO\BasicBundle\Table;
 
 
 /**
- * DESCRIPTION
+ * General Table Class
  *
+ * @package IIDO\BasicBundle\Table
  * @author Stephan Preßl <https://github.com/pressi>
  */
 class AllTables
 {
 
-//	/**
-//	 * Return the link picker wizard
-//	 * @param \DataContainer
-//	 * @return string
-//	 */
-//	public function pagePicker($dc)
-//	{
-//		$strId				= 0;
-//		$strTable			= "";
-//		$strField			= "";
-//		$strValue			= "";
-//		$strIField			= "";
-//
-//		if( is_array($dc) )
-//		{
-//			$strId			= $dc['id'];
-//			$strField		= $dc['field'];
-//			$strTable		= $dc['table'];
-//			$strValue		= $dc['value'];
-//
-////			$strIField		= (($dc['ifield']) ? $dc['ifield'] : $strField);
-//			$strIField		= $strField;
-//		}
-//		elseif($dc instanceof \DataContainer)
-//		{
-//			$strId 			= $dc->id;
-//			$strField		= $dc->field;
-//			$strTable		= $dc->table;
-//			$strValue		= $dc->value;
-//
-////			$strIField		= (($dc->ifield) ? $dc->iFfeld : $strField);
-//			$strIField		= $strField;
-//		}
-//
-//		$strTitle	= specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']);
-//
-//		return ' <a href="contao/page.php?do=' . \Input::get('do') . '&amp;table=' . $strTable . '&amp;field=' . $strIField . '&amp;value=' . str_replace(array('{{link_url::', '}}'), '', $strValue) . '" title="' . $strTitle . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\'' . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MOD']['page'][0])) . '\',\'url\':this.href,\'id\':\'' . $strField . '\',\'tag\':\'ctrl_'. $strIField . ((\Input::get('act') == 'editAll') ? '_' . $strId : '') . '\',\'self\':this});return false">' . \Image::getHtml('pickpage.gif', $GLOBALS['TL_LANG']['MSC']['pagepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
-//	}
-
-
 
     /**
      * Strip all not allowed tags
+     *
      * @param string $varValue
      * @param \DataContainer $dc
+     *
      * @return string
      */
     public function strip_all_tags($varValue, \DataContainer $dc)
     {
         $allowedTags = '<strong></strong><em></em><span></span><light></light><br>';
 
-        if(is_array(deserialize($varValue)))
+        if(is_array(\StringUtil::deserialize($varValue)))
         {
-            $varValue           = deserialize($varValue);
+            $varValue           = \StringUtil::deserialize($varValue);
             $varValue['value']  = strip_tags($varValue['value'], $allowedTags);
             $varValue           = serialize($varValue);
         }
@@ -85,44 +48,6 @@ class AllTables
 
         return $varValue;
     }
-
-
-
-//	/**
-//	 * Return the link picker wizard
-//	 *
-//	 * @param \DataContainer $dc
-//	 *
-//	 * @return string
-//	 */
-//	public function pagePicker($dc)
-//	{
-//		$href		= "";
-//		$table		= "";
-//		$field		= "";
-//		$id			= 0;
-//		$value		= "";
-//
-//		if($dc instanceof \DataContainer)
-//		{
-//			$href		= (($dc->value == '' || strpos($dc->value, '{{link_url::') !== false) ? 'contao/page' : 'contao/file');
-//			$table		= $dc->table;
-//			$field		= $dc->field;
-//			$id			= $dc->id;
-//			$value		= rawurlencode(str_replace(array('{{link_url::', '}}'), '', $dc->value));
-//		}
-//		elseif( is_array($dc) )
-//		{
-//			$href		= (($dc['value'] == '' || strpos($dc['value'], '{{link_url::') !== false) ? 'contao/page' : 'contao/file');
-//			$table		= $dc['table'];
-//			$field		= $dc['field'];
-//			$id			= $dc['id'];
-//			$value		= rawurlencode(str_replace(array('{{link_url::', '}}'), '', $dc['value']));
-//		}
-//
-//
-//		return ' <a href="' . $href . '?do=' . \Input::get('do') . '&amp;table=' . $table . '&amp;field=' . $field . '&amp;value=' . $value . '&amp;switch=1' . '" title="' . \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['pagepicker']) . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':768,\'title\':\'' . \StringUtil::specialchars(str_replace("'", "\\'", $GLOBALS['TL_DCA'][$table]['fields'][$field]['label'][0])) . '\',\'url\':this.href,\'id\':\'' . $field . '\',\'tag\':\'ctrl_'. $field . ((\Input::get('act') == 'editAll') ? '_' . $id : '') . '\',\'self\':this});return false;">' . \Image::getHtml('pickpage.svg', $GLOBALS['TL_LANG']['MSC']['pagepicker']) . '</a>';
-//	}
 
 
     /**
@@ -170,44 +95,47 @@ class AllTables
     }
 
 
-	/**
-	 * Return the file picker wizard
-	 * @param \DataContainer
-	 * @return string
-	 */
-	public function filePicker($dc)
-	{
-		$strId				= 0;
-		$strTable			= "";
-		$strField			= "";
-		$strValue			= "";
-		$strIField			= "";
 
-		if( is_array($dc) )
-		{
-			$strId			= $dc['id'];
-			$strField		= $dc['field'];
-			$strTable		= $dc['table'];
-			$strValue		= $dc['value'];
+    /**
+     * Return the file picker wizard
+     *
+     * @param \DataContainer|array $dc
+     *
+     * @return string
+     */
+    public function filePicker($dc)
+    {
+        $strId      = 0;
+        $strTable   = "";
+        $strField   = "";
+        $strValue   = "";
+        $strIField  = "";
 
-			$strIField		= (($dc['ifield']) ? $dc['ifield'] : $strField);
-		}
-		elseif($dc instanceof \DataContainer)
-		{
-			$strId 			= $dc->id;
-			$strField		= $dc->field;
-			$strTable		= $dc->table;
-			$strValue		= $dc->value;
+        if( is_array($dc) )
+        {
+            $strId      = $dc['id'];
+            $strField   = $dc['field'];
+            $strTable   = $dc['table'];
+            $strValue   = $dc['value'];
 
-			$strIField		= (($dc->ifield) ? $dc->iFfeld : $strField);
-		}
+            $strIField  = (($dc['ifield']) ? $dc['ifield'] : $strField);
+        }
+        elseif($dc instanceof \DataContainer)
+        {
+            $strId      = $dc->id;
+            $strField   = $dc->field;
+            $strTable   = $dc->table;
+            $strValue   = $dc->value;
 
-		$strTitle 	= specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker']));
-		$onClick	= "Backend.getScrollOffset();Backend.openModalSelector({'width':765,'title':'" . specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]) . "','url':this.href,'id':'" . $strField. "','tag':'ctrl_" . $strIField . ((\Input::get('act') == 'editAll') ? '_' . $strId : '') . "','self':this});return false";
-		$strImage	= \Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"');
+            $strIField  = (($dc->ifield) ? $dc->iFfeld : $strField);
+        }
 
-		return ' <a href="contao/file.php?do=' . \Input::get('do') . '&amp;table=' . $strTable . '&amp;field=' . $strIField . '&amp;value=' . $strValue . '" title="' . $strTitle . '" onclick="' . $onClick . '">' . $strImage . '</a>';
-	}
+        $strTitle   = specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker']));
+        $onClick    = "Backend.getScrollOffset();Backend.openModalSelector({'width':765,'title':'" . specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]) . "','url':this.href,'id':'" . $strField. "','tag':'ctrl_" . $strIField . ((\Input::get('act') == 'editAll') ? '_' . $strId : '') . "','self':this});return false";
+        $strImage   = \Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"');
+
+        return ' <a href="contao/file.php?do=' . \Input::get('do') . '&amp;table=' . $strTable . '&amp;field=' . $strIField . '&amp;value=' . $strValue . '" title="' . $strTitle . '" onclick="' . $onClick . '">' . $strImage . '</a>';
+    }
 
 
 
@@ -222,50 +150,53 @@ class AllTables
 
 
 
-	/**
-	 * Pre-fill the "alt" and "caption" fields with the file meta data
-	 * @param mixed
-	 * @param \DataContainer
-	 * @return mixed
-	 */
-	public function storeFileMetaInformation($varValue, \DataContainer $dc)
-	{
-		if ($dc->activeRecord->singleSRC == $varValue)
-		{
-			return $varValue;
-		}
+    /**
+     * Pre-fill the "alt" and "caption" fields with the file meta data
+     *
+     * @param mixed $varValue
+     * @param \DataContainer $dc
+     *
+     * @return mixed
+     */
+    public function storeFileMetaInformation($varValue, \DataContainer $dc)
+    {
+        if ($dc->activeRecord->singleSRC === $varValue)
+        {
+            return $varValue;
+        }
 
-		$objFile = \FilesModel::findByUuid($varValue);
+        $objFile = \FilesModel::findByUuid($varValue);
 
-		if ($objFile !== null)
-		{
-			$arrMeta = deserialize($objFile->meta);
+        if ($objFile !== null)
+        {
+            $arrMeta = deserialize($objFile->meta);
 
-			if (!empty($arrMeta))
-			{
-				$strLanguage = \System::getContainer()->get('request_stack')->getCurrentRequest()->getLocale();
+            if (!empty($arrMeta))
+            {
+                $strLanguage = \System::getContainer()->get('request_stack')->getCurrentRequest()->getLocale();
 
-				if ( isset($arrMeta[$strLanguage]) )
-				{
-					\Input::setPost('alt', $arrMeta[$strLanguage]['title']);
-					\Input::setPost('caption', $arrMeta[$strLanguage]['caption']);
-				}
-			}
-		}
+                if ( isset($arrMeta[$strLanguage]) )
+                {
+                    \Input::setPost('alt', $arrMeta[$strLanguage]['title']);
+                    \Input::setPost('caption', $arrMeta[$strLanguage]['caption']);
+                }
+            }
+        }
 
-		return $varValue;
-	}
+        return $varValue;
+    }
 
 
 
-	/**
-	 * Generates a filePicker icon for Contao Version > 3.1
-	 * @param \DataContainer $dc
-	 *
-	 * @return string
-	 */
-	public function rewFilePicker(\DataContainer $dc)
-	{
-		return ' <a href="contao/file.php?do='.\Input::get('do').'&amp;table='.$dc->table.'&amp;field='.preg_replace('/_row[0-9]*_/i', '__', $dc->field).'&amp;value='.$dc->value.'" title="'.specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker'])).'" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\''.specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]).'\',\'url\':this.href,\'id\':\''.$dc->field.'\',\'tag\':\'ctrl_'.$dc->field . ((\Input::get('act') == 'editAll') ? '_' . $dc->id : '').'\',\'self\':this});return false">' . \Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
-	}
+    /**
+     * Generates a filePicker icon for Contao Version > 3.1
+     *
+     * @param \DataContainer $dc
+     *
+     * @return string
+     */
+    public function rewFilePicker(\DataContainer $dc)
+    {
+        return ' <a href="contao/file.php?do='.\Input::get('do').'&amp;table='.$dc->table.'&amp;field='.preg_replace('/_row[0-9]*_/i', '__', $dc->field).'&amp;value='.$dc->value.'" title="'.specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker'])).'" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\''.specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]).'\',\'url\':this.href,\'id\':\''.$dc->field.'\',\'tag\':\'ctrl_'.$dc->field . ((\Input::get('act') == 'editAll') ? '_' . $dc->id : '').'\',\'self\':this});return false">' . \Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
+    }
 }
--- Table/ArticleTable.php
+++ Table/ArticleTable.php
@@ -1,27 +1,45 @@
 <?php
 /*******************************************************************
+ *
  * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
  * All rights reserved
+ *
  * Modification, distribution or any other action on or with
  * this file is permitted unless explicitly granted by IIDO
  * www.iido.at <development@iido.at>
+ *
  *******************************************************************/
 
 namespace IIDO\BasicBundle\Table;
 
-use IIDO\BasicBundle\Helper\BasicHelper;
 
 /**
  * Class Content Table
+ *
  * @package IIDO\BasicBundle\Table
+ * @author Stephan Preßl <https://github.com/pressi>
  */
 class ArticleTable extends \Backend
 {
 
-    protected $strTable             = 'tl_article';
+    /**
+     * Table Name
+     */
+    protected $strTable = 'tl_article';
 
 
-
+    /**
+     * Return the edit article button
+     *
+     * @param array $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
     public function editArticle( $row, $href, $label, $title, $icon, $attributes )
     {
         $objClass   = new \tl_article();
