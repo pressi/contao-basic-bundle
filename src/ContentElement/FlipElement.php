9a5231ba6b9d89c1b961d2f6b39a019c913d9c1e
Stephan Preßl <mail@stephanpressl.at>
2018-10-13 10:52:25 +0200

    add flip content element

---
+++ src/ContentElement/FlipElement.php
@@ -0,0 +1,38 @@
+<?php
+/*******************************************************************
+ * (c) 2018 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
+
+namespace IIDO\BasicBundle\ContentElement;
+
+
+/**
+ * Front end content element "flip".
+ *
+ * @package IIDO\BasicBundle
+ * @author Stephan Preßl <development@prestep.at>
+ */
+class FlipElement extends \ContentElement
+{
+
+    /**
+     * Template
+     *
+     * @var string
+     */
+    protected $strTemplate = 'ce_iido_flip';
+
+
+
+    /**
+     * Generate the content element
+     */
+    protected function compile()
+    {
+
+    }
+}
