3a0305e3fb624b4683ef3825b51ff4ea738a365e
pressi <mail@stephanpressl.at>
2020-07-24 12:52:06 +0200

    Update NewsListener.php

--- src/EventListener/DataContainer/NewsListener.php
+++ src/EventListener/DataContainer/NewsListener.php
@@ -1,26 +1,35 @@
 <?php
-
-/*
- * News Categories bundle for Contao Open Source CMS.
- *
- * @copyright  Copyright (c) 2017, Codefog
- * @author     Codefog <https://codefog.pl>
- * @license    MIT
- */
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
 
 namespace IIDO\BasicBundle\EventListener\DataContainer;
 
+
+use Codefog\NewsCategoriesBundle\Criteria\NewsCriteria;
+use Codefog\NewsCategoriesBundle\Model\NewsCategoryModel;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\ServiceAnnotation\Hook;
 use Contao\DataContainer;
-use Contao\NewsArchiveModel;
+use Haste\Model\Model;
+use Contao\Model\Collection;
+use Contao\Module;
+use Contao\NewsModel;
 use Contao\System;
-use IIDO\BasicBundle\Permission\NewsPermissionChecker as PermissionChecker;
+use Contao\Input;
 use Contao\CoreBundle\Framework\FrameworkAwareInterface;
 use Contao\CoreBundle\Framework\FrameworkAwareTrait;
-use Contao\Input;
 use Doctrine\DBAL\Connection;
+use IIDO\BasicBundle\Helper\BasicHelper;
+use Terminal42\ServiceAnnotationBundle\ServiceAnnotationInterface;
+use IIDO\BasicBundle\Permission\NewsPermissionChecker as PermissionChecker;
 
-class NewsListener implements FrameworkAwareInterface
+
+class NewsListener implements FrameworkAwareInterface, ServiceAnnotationInterface
 {
     use FrameworkAwareTrait;
 
@@ -214,5 +223,140 @@ class NewsListener implements FrameworkAwareInterface
         }
 
         return $varValue;
+    }
+
+
+
+    /**
+     * @Hook("newsListFetchItems", priority=10)
+     */
+    public function onNewsListFetchItems( array $newsArchives, ?bool $featuredOnly, int $limit, int $offset, Module $module )
+    {
+
+        // check if news archives are defined
+        if( !$newsArchives )
+        {
+            return null;
+        }
+
+        // support for news_categories
+        if( !BasicHelper::isActiveBundle('codefog/contao-news_categories') )
+        {
+            return false;
+        }
+
+        $module->news_order = $module->news_order ?: $module->news_sorting;
+
+//        if( $module->news_order !== 'order_categoryGroup' )
+//        {
+//            return false;
+//        }
+
+        $objCategories  = NewsCategoryModel::findPublishedByPid( 1 ); //TODO: im backend verwaltbar machen
+        $arrNews        = [];
+        $arrShown       = [];
+
+        if( $objCategories )
+        {
+            while( $objCategories->next() )
+            {
+                $objCategory = $objCategories->current();
+//                $objCategory->isHeader = true;
+
+                /** @var Model $model */
+                $model = $this->framework->getAdapter(Model::class);
+
+                $newsIds = $model->getReferenceValues('tl_news', 'categories', [$objCategory->id]);
+                $newsIds = $this->parseIds($newsIds);
+
+                if (0 === \count($newsIds))
+                {
+                    continue;
+                }
+//                echo "<pre>"; print_r( $objCategory->title );
+//                echo "<br>"; print_r( $newsIds ); echo "</pre>";
+
+//                if( $newsIds && count($newsIds) )
+//                {
+//                    $arrNews[] = $objCategory;
+
+                //TODO: save news in array / keine weitere datenbankabfrage für die selbe news!!
+                    $i = 0;
+                    foreach( $newsIds as $newsId )
+                    {
+                        $objNews = NewsModel::findByPk( $newsId );
+                        $objNews = clone $objNews;
+
+                        if( $i === 0 )
+                        {
+                            $objNews->hasHeader = true;
+                            $objNews->headerCategory = $objCategory;
+                        }
+                        else
+                        {
+                            $objNews->hasHeader = false;
+                        }
+
+                        $arrShown[] = $newsId;
+                        $arrNews[] = $objNews;
+
+                        $i++;
+                    }
+//                }
+//echo "<pre>"; print_r( $newsIds ); exit;
+//                $criteria = new NewsCriteria( $this->framework );
+
+//                $criteria->setBasicCriteria($newsArchives, $module->news_order, $featuredOnly);
+//                $criteria->setDefaultCategories([$objCategory->id]);
+
+//                $arrCatNews = $criteria->getNewsModelAdapter()->findBy(
+//                    $criteria->getColumns(),
+//                    $criteria->getValues(),
+//                    $criteria->getOptions()
+//                );
+
+//                if( $arrCatNews )
+//                {
+//                    $arrNews[] = $objCategory;
+//
+//                    while( $arrCatNews->next() )
+//                    {
+//                        $arrNews[] = $arrCatNews->current();
+//                    }
+//                }
+            }
+        }
+//        echo "<pre>"; print_r( $arrNews ); exit;
+
+        if( count($arrNews) )
+        {
+            $collection = new Collection( $arrNews, 'tl_news' );
+
+//            echo "<pre>"; print_r( count($arrNews) );
+//            echo "<br>"; print_r( $collection->count() );
+//            exit;
+
+            return $collection;
+        }
+
+        return false;
+    }
+
+
+
+    /**
+     * Parse the record IDs.
+     *
+     * @param array $ids
+     *
+     * @return array
+     */
+    private function parseIds(array $ids)
+    {
+        $ids = \array_map('intval', $ids);
+        $ids = \array_filter($ids);
+        $ids = \array_unique($ids);
+
+        return \array_values($ids);
     }
 }
