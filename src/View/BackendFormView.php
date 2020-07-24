a1b8b96aa37d9afb1cf2046b5e6731974e361328
pressi <mail@stephanpressl.at>
2020-07-24 12:51:44 +0200

    Update BackendFormView.php

--- src/View/BackendFormView.php
+++ src/View/BackendFormView.php
@@ -1,5 +1,11 @@
 <?php declare(strict_types=1);
-
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
 
 namespace IIDO\BasicBundle\View;
 
