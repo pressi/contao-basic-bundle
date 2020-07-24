f85064ed41bf2150da79dd5c921f0c8d46ce64a6
pressi <mail@stephanpressl.at>
2020-07-24 12:50:34 +0200

    Update ColumnMasterElement.php

--- src/Controller/Elements/ColumnMasterElement.php
+++ src/Controller/Elements/ColumnMasterElement.php
@@ -1,5 +1,11 @@
 <?php
-
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
 
 namespace IIDO\BasicBundle\Controller\Elements;
 
