38747a22be56283b6806fdc4cfbdf34db07bc9b1
pressi <mail@stephanpressl.at>
2021-01-12 10:25:09 +0100

    Create ColorUtil.php

---
+++ src/Util/ColorUtil.php
@@ -0,0 +1,26 @@
+<?php
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
+
+namespace IIDO\BasicBundle\Util;
+
+
+use Contao\DataContainer;
+
+
+class ColorUtil
+{
+
+    //TODO:
+    public static function getWebsiteColors( ?DataContainer $dc ): array
+    {
+        $colors = [];
+
+        return $colors;
+    }
+}
